068fd331fabea8da459c4223274db2a92763275d
Rafik Djedjig <rafik.djedjig@web-education.net>
2017-09-08 18:38:11 +0200

    [Feeder] [fix] Change soft errors report to ease context error highlight

--- feeder/src/main/java/org/entcore/feeder/utils/Validator.java
+++ feeder/src/main/java/org/entcore/feeder/utils/Validator.java
@@ -459,7 +459,7 @@ public class Validator {
 				if (errorsContext != null) {
 					errorsContext.add(new JsonObject()
 							.put("reason", "invalid.value")
-							.put("attribute", i18n.translate(attr, I18n.DEFAULT_DOMAIN, acceptLanguage))
+							.put("attribute", attr)
 							.put("value", (value != null ? value.toString() : "null"))
 					);
 				}
