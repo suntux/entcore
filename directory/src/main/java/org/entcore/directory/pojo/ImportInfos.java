rdjedjig/entcore-mirror

[Evo] CAV2-193 : classes mapping

directory/src/main/java/org/entcore/directory/pojo/ImportInfos.java
@@ -49,6 +49,7 @@ public class ImportInfos {
 	private String structureId;
 	private String structureExternalId;
 	private Map<String, Object> mappings;
+	private Map<String, Object> classesMapping;
 	private String language;
 
 	public String getFeeder() {
@@ -137,6 +138,14 @@ public class ImportInfos {
 
 	public void setMappings(JsonObject mappings) {
 		this.mappings = (mappings != null) ? mappings.toMap() : null;
+	}
+
+	public Map<String, Object> getClassesMapping() {
+		return classesMapping;
+	}
+
+	public void setClassesMapping(JsonObject classesMapping) {
+		this.classesMapping = (classesMapping != null) ? classesMapping.toMap() : null;
 	}
 
 	public void validate(final boolean isAdmc, final Vertx vertx, final Handler<AsyncResult<String>> handler) {

directory/src/main/java/org/entcore/directory/services/impl/DefaultImportService.java
@@ -135,6 +135,19 @@ public class DefaultImportService implements ImportService {
 	}
 
 	@Override
+	public void classesMapping(ImportInfos importInfos, final Handler<Either<JsonObject, JsonObject>> handler) {
+		try {
+			JsonObject action = new JsonObject(mapper.writeValueAsString(importInfos))
+					.putString("action", "classesMapping");
+			sendCommand(handler, action);
+		} catch (JsonProcessingException e) {
+			handler.handle(new Either.Left<JsonObject, JsonObject>(new JsonObject()
+					.putArray("global", new JsonArray().addString("unexpected.error"))));
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
 	public void addLine(String importId, String profile, JsonObject line, Handler<Either<String, JsonObject>> handler) {
 		final JsonObject query = new JsonObject().putString("_id", importId);
 		final JsonObject update = new JsonObject().putObject("$push", new JsonObject().putObject("files." + profile, line));

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -286,6 +286,8 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 				break;
 			case "columnsMapping" : csvColumnMapping(message);
 				break;
+			case "classesMapping" : csvClassesMapping(message);
+				break;
 			case "ignore-duplicate" :
 				duplicateUsers.ignoreDuplicate(message);
 				break;
@@ -322,6 +324,22 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 				sendError(message, "invalid.action");
 		}
 		checkEventQueue();
+	}
+
+	private void csvClassesMapping(final Message<JsonObject> message) {
+		final CsvValidator v = new CsvValidator(vertx, message.body());
+		String path = message.body().getString("path");
+		v.classesMapping(path, new Handler<JsonObject>() {
+			@Override
+			public void handle(JsonObject event) {
+				if (!v.containsErrors()) {
+					sendOK(message, new JsonObject()
+							.putObject("classesMapping", v.getClassesMappings()));
+				} else {
+					sendError(message, "classes.mapping.error");
+				}
+			}
+		});
 	}
 
 	private void importWithId(final Message<JsonObject> message) {
@@ -424,12 +442,12 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 			@Override
 			public void handle(final JsonObject result) {
 				final Report r = (Report) v;
-				r.persist(new Handler<Message<JsonObject>>() {
+				final Handler<Message<JsonObject>> persistHandler = new Handler<Message<JsonObject>>() {
 					@Override
 					public void handle(Message<JsonObject> event) {
 
 					}
-				});
+				};
 				if (preDelete && structureExternalId != null && !r.containsErrors()) {
 					final JsonArray externalIds = r.getUsersExternalId();
 					new User.PreDeleteTask(0).findMissingUsersInStructure(
@@ -454,6 +472,7 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 							} else {
 								sendOK(message, new JsonObject().putObject("result", r.getResult()));
 							}
+							r.persist(persistHandler);
 						}
 					});
 				} else {
@@ -462,6 +481,7 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 					} else {
 						sendOK(message, new JsonObject().putObject("result", r.getResult()));
 					}
+					r.persist(persistHandler);
 				}
 			}
 		});

feeder/src/main/java/org/entcore/feeder/csv/CsvReport.java
@@ -42,7 +42,9 @@ import static fr.wseduc.webutils.Utils.isNotEmpty;
 public class CsvReport extends Report {
 
 	private static final String MAPPINGS = "mappings";
+	private static final String CLASSES_MAPPING = "classesMapping";
 	private static final String HEADERS = "headers";
+	public static final String KEYS_CLEANED = "keysCleaned";
 	private final Vertx vertx;
 	protected final ProfileColumnsMapper columnsMapper;
 
@@ -54,6 +56,9 @@ public class CsvReport extends Report {
 			importInfos.removeField("id");
 		}
 		result.mergeIn(importInfos);
+		if (result.getBoolean(KEYS_CLEANED, false)) {
+			uncleanKeys();
+		}
 		this.vertx = vertx;
 		this.columnsMapper = new ProfileColumnsMapper(getMappings());
 	}
@@ -84,6 +89,24 @@ public class CsvReport extends Report {
 		if (mappings != null && mappings.size() > 0) {
 			result.putObject(MAPPINGS, mappings);
 		}
+	}
+
+	public void setClassesMapping(JsonObject mapping) {
+		if (mapping != null && mapping.size() > 0) {
+			result.putObject(CLASSES_MAPPING, mapping);
+		}
+	}
+
+	public JsonObject getClassesMapping(String profile) {
+		final JsonObject cm = result.getObject(CLASSES_MAPPING);
+		if (cm != null) {
+			return cm.getObject(profile);
+		}
+		return null;
+	}
+
+	public JsonObject getClassesMappings() {
+		return result.getObject(CLASSES_MAPPING);
 	}
 
 	public void exportFiles(final Handler<AsyncResult<String>> handler) {
@@ -176,6 +199,26 @@ public class CsvReport extends Report {
 	@Override
 	public String getSource() {
 		return "CSV";
+	}
+
+	public String getStructureExternalId() {
+		return result.getString("structureExternalId");
+	}
+
+	@Override
+	protected void cleanKeys() {
+		int count = 0;
+		count += cleanAttributeKeys(getClassesMappings());
+		count += cleanAttributeKeys(getMappings());
+		if (count > 0) {
+			result.putBoolean(KEYS_CLEANED, true);
+		}
+	}
+
+	protected void uncleanKeys() {
+		uncleanAttributeKeys(getClassesMappings());
+		uncleanAttributeKeys(getMappings());
+		result.removeField(KEYS_CLEANED);
 	}
 
 //	protected void setStructureExternalIdIfAbsent(String structureExternalId) {

feeder/src/main/java/org/entcore/feeder/csv/CsvValidator.java
@@ -38,6 +38,8 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 
+import static fr.wseduc.webutils.Utils.getOrElse;
+import static fr.wseduc.webutils.Utils.isNotEmpty;
 import static org.entcore.feeder.csv.CsvFeeder.frenchDatePatter;
 import static org.entcore.feeder.csv.CsvFeeder.generateUserExternalId;
 import static org.entcore.feeder.csv.CsvFeeder.relativeStudentMapping;
@@ -45,12 +47,13 @@ import static org.entcore.feeder.utils.CSVUtil.getCsvReader;
 
 public class CsvValidator extends CsvReport implements ImportValidator {
 
-	private enum CsvValidationProcessType { VALIDATE, COLUMN_MAPPING }
+	private enum CsvValidationProcessType { VALIDATE, COLUMN_MAPPING, CLASSES_MAPPING }
 	private final Vertx vertx;
 	private String structureId;
 	private final MappingFinder mappingFinder;
 	private boolean findUsersEnabled = true;
 	private final Map<String, String> classesNamesMapping = new HashMap<>();
+	private final Map<String, Set<String>> profilesClassesMapping = new HashMap<>();
 	public static final Map<String, Validator> profiles;
 
 	static {
@@ -71,6 +74,76 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 
 	public void columnsMapping(final String p, final Handler<JsonObject> handler) {
 		process(p, CsvValidationProcessType.COLUMN_MAPPING, handler);
+	}
+
+	public void classesMapping(final String p, final Handler<JsonObject> handler) {
+		process(p, CsvValidationProcessType.CLASSES_MAPPING, new Handler<JsonObject>() {
+			@Override
+			public void handle(JsonObject event) {
+				if (isNotEmpty(getStructureExternalId())) {
+					final String query =
+							"MATCH (s:Structure {externalId:{externalId}})<-[:BELONGS]-(c:Class) " +
+							"RETURN COLLECT(DISTINCT c.name) as classes";
+					final JsonObject params = new JsonObject().putString("externalId", getStructureExternalId());
+					TransactionManager.getNeo4jHelper().execute(query, params, new Handler<Message<JsonObject>>() {
+						@Override
+						public void handle(Message<JsonObject> event) {
+							final JsonArray r = event.body().getArray("result");
+							if ("ok".equals(event.body().getString("status")) && r != null && r.size() > 0 && r.get(0) != null) {
+								final JsonArray classes = r.<JsonObject>get(0).getArray("classes");
+								mapClasses(classes);
+							} else {
+								addError("error.database.classes");
+								log.error(event.body().getString("message"));
+							}
+							handler.handle(result);
+						}
+					});
+				} else {
+					mapClasses(null);
+					handler.handle(result);
+				}
+			}
+		});
+	}
+
+	protected void mapClasses(JsonArray classes) {
+		JsonObject classesMapping = new JsonObject();
+		Set<String> stm = profilesClassesMapping.get("Student");
+		if (stm != null && stm.size() > 0) {
+			for (String profile : profiles.keySet()) {
+				if (!"Student".equals(profile)) {
+					Set<String> m = profilesClassesMapping.get(profile);
+					if (m == null || m.size() == 0) continue;
+					JsonObject jm = new JsonObject();
+					classesMapping.putObject(profile, jm);
+					for (String c: m) {
+						if (stm.contains(c)) {
+							jm.putString(c, c);
+						} else {
+							jm.putString(c, "");
+						}
+					}
+				}
+			}
+			JsonObject jm = new JsonObject();
+			classesMapping.putObject("Student", jm);
+			for (String s : stm) {
+				jm.putString(s, "");
+			}
+		}
+		if (classes != null && classes.size() > 0) {
+			for (String attr : classesMapping.getFieldNames()) {
+				JsonObject j = classesMapping.getObject(attr);
+				for (String attr2 : j.getFieldNames()) {
+					if (classes.contains(attr2)) {
+						j.putString(attr2, attr2);
+					}
+				}
+			}
+			classesMapping.putArray("dbClasses", classes);
+		}
+		setClassesMapping(classesMapping);
 	}
 
 	@Override
@@ -164,6 +237,9 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 															case COLUMN_MAPPING:
 																checkColumnsMapping(file, profile, charset, h);
 																break;
+															case CLASSES_MAPPING:
+																checkClassesMapping(file, profile, charset, h);
+																break;
 														}
 													} else {
 														addError("unknown.profile");
@@ -187,6 +263,51 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 				}
 			}
 		});
+	}
+
+	private void checkClassesMapping(String path, String profile, String charset, Handler<JsonObject> handler) {
+		try {
+			CSVReader csvParser = getCsvReader(path, charset);
+
+			String[] strings;
+			final List<String> columns = new ArrayList<>();
+			final List<Integer> classesIdx = new ArrayList<>();
+			final Set<String> mapping = new HashSet<>();
+			profilesClassesMapping.put(profile, mapping);
+			int i = 0;
+			while ((strings = csvParser.readNext()) != null) {
+				if (i == 0) {
+					JsonArray invalidColumns = columnsMapper.getColumsNames(profile, strings, columns);
+					if (invalidColumns.size() > 0 ) {
+						parseErrors("invalid.column", invalidColumns, profile, handler);
+						return;
+					} else if (!columns.contains("classes") && !columns.contains("childClasses")) {
+						handler.handle(result);
+						return;
+					} else {
+						int j = 0;
+						for (String column : columns) {
+							if ("classes".equals(column) || "childClasses".equals(column)) {
+								classesIdx.add(j);
+							}
+							j++;
+						}
+					}
+				} else {
+					for (Integer idx : classesIdx) {
+						if (isNotEmpty(strings[idx].trim())) {
+							mapping.add(strings[idx].trim());
+						}
+					}
+				}
+				i++;
+			}
+		} catch (Exception e) {
+			addError(profile, "csv.exception");
+			log.error("csv.exception", e);
+		} finally {
+			handler.handle(result);
+		}
 	}
 
 	private void checkColumnsMapping(String path, String profile, String charset, Handler<JsonObject> handler) {
@@ -333,6 +454,7 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 				}
 //				setStructureExternalIdIfAbsent(structure.getExternalId());
 				try {
+					final JsonObject classMapping = getClassesMapping(profile);
 					CSVReader csvParser = getCsvReader(path, charset, 1);
 					String[] strings;
 					int i = 1;
@@ -349,9 +471,15 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 								return;
 							}
 							final String c = columns.get(j);
-							final String v = strings[j].trim();
+							final String v;
+							if ("classes".equals(c) && classMapping != null) {
+								v = getOrElse(classMapping.getString(strings[j].trim()), strings[j].trim(), false);
+							} else {
+								v = strings[j].trim();
+							}
 							if ((v.isEmpty() && !"childUsername".equals(c)) ||
 									(v.isEmpty() && "childUsername".equals(c) && strings[j+1].trim().isEmpty())) continue;
+
 							switch (validator.getType(c)) {
 								case "string":
 									if ("birthDate".equals(c)) {

feeder/src/main/java/org/entcore/feeder/utils/Report.java
@@ -111,7 +111,7 @@ public class Report {
 	}
 
 	public JsonObject getResult() {
-		return result;
+		return result.copy();
 	}
 
 	public JsonArray getUsersExternalId() {
@@ -136,7 +136,43 @@ public class Report {
 	}
 
 	public void persist(Handler<Message<JsonObject>> handler) {
+		cleanKeys();
 		MongoDb.getInstance().save("imports", this.getResult(), handler);
+	}
+
+	protected void cleanKeys() {}
+
+	protected int cleanAttributeKeys(JsonObject attribute) {
+		int count = 0;
+		if (attribute != null) {
+			for (String attr : attribute.getFieldNames()) {
+				JsonObject j = attribute.getObject(attr);
+				if (j != null) {
+					for (String attr2 : j.copy().getFieldNames()) {
+						if (attr2.contains(".")) {
+							count++;
+							j.putString(attr2.replaceAll("\\.", "_|_"), (String) j.removeField(attr2));
+						}
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	protected void uncleanAttributeKeys(JsonObject attribute) {
+		if (attribute != null) {
+			for (String attr : attribute.getFieldNames()) {
+				JsonObject j = attribute.getObject(attr);
+				if (j != null) {
+					for (String attr2 : j.copy().getFieldNames()) {
+						if (attr2.contains("_|_")) {
+							j.putString(attr2.replaceAll("_\\|_", "."), (String) j.removeField(attr2));
+						}
+					}
+				}
+			}
+		}
 	}
 
 	public String getSource() {

suntux/entcore

directory/src/main/java/org/entcore/directory/pojo/ImportInfos.java
@@ -51,6 +51,7 @@ public class ImportInfos {
 	private String structureExternalId;
 	private String overrideClass;
 	private Map<String, Object> mappings;
+	private Map<String, Object> classesMapping;
 	private String language;
 
 	public String getFeeder() {
@@ -147,6 +148,14 @@ public class ImportInfos {
 
 	public void setMappings(JsonObject mappings) {
 		this.mappings = (mappings != null) ? mappings.getMap() : null;
+	}
+
+	public Map<String, Object> getClassesMapping() {
+		return classesMapping;
+	}
+
+	public void setClassesMapping(JsonObject classesMapping) {
+		this.classesMapping = (classesMapping != null) ? classesMapping.getMap() : null;
 	}
 
 	public void validate(final boolean isAdmc, final Vertx vertx, final Handler<AsyncResult<String>> handler) {

directory/src/main/java/org/entcore/directory/services/impl/DefaultImportService.java
@@ -159,6 +159,19 @@ public class DefaultImportService implements ImportService {
 	}
 
 	@Override
+	public void classesMapping(ImportInfos importInfos, final Handler<Either<JsonObject, JsonObject>> handler) {
+		try {
+			JsonObject action = new JsonObject(mapper.writeValueAsString(importInfos))
+					.put("action", "classesMapping");
+			sendCommand(handler, action);
+		} catch (JsonProcessingException e) {
+			handler.handle(new Either.Left<JsonObject, JsonObject>(new JsonObject()
+					.put("global", new JsonArray().add("unexpected.error"))));
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
 	public void addLine(String importId, String profile, JsonObject line, Handler<Either<String, JsonObject>> handler) {
 		final JsonObject query = new JsonObject().put("_id", importId);
 		final JsonObject update = new JsonObject().put("$push", new JsonObject().put("files." + profile, line));

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -295,6 +295,8 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 				break;
 			case "columnsMapping" : csvColumnMapping(message);
 				break;
+			case "classesMapping" : csvClassesMapping(message);
+				break;
 			case "ignore-duplicate" :
 				duplicateUsers.ignoreDuplicate(message);
 				break;
@@ -334,6 +336,22 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 				sendError(message, "invalid.action");
 		}
 		checkEventQueue();
+	}
+
+	private void csvClassesMapping(final Message<JsonObject> message) {
+		final CsvValidator v = new CsvValidator(vertx, message.body().getString("langage"),message.body());
+		String path = message.body().getString("path");
+		v.classesMapping(path, new Handler<JsonObject>() {
+			@Override
+			public void handle(JsonObject event) {
+				if (!v.containsErrors()) {
+					sendOK(message, new JsonObject()
+							.put("classesMapping", v.getClassesMappings()));
+				} else {
+					sendError(message, "classes.mapping.error");
+				}
+			}
+		});
 	}
 
 	private void importWithId(final Message<JsonObject> message) {
@@ -438,12 +456,12 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 			@Override
 			public void handle(final JsonObject result) {
 				final Report r = (Report) v;
-				r.persist(new Handler<Message<JsonObject>>() {
+				final Handler<Message<JsonObject>> persistHandler = new Handler<Message<JsonObject>>() {
 					@Override
 					public void handle(Message<JsonObject> event) {
 
 					}
-				});
+				};
 				if (preDelete && structureExternalId != null && !r.containsErrors()) {
 					final JsonArray externalIds = r.getUsersExternalId();
 					final JsonArray profiles = r.getResult().getJsonArray(Report.PROFILES);
@@ -469,6 +487,7 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 							} else {
 								sendOK(message, new JsonObject().put("result", r.getResult()));
 							}
+							r.persist(persistHandler);
 						}
 					});
 				} else {
@@ -477,6 +496,7 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 					} else {
 						sendOK(message, new JsonObject().put("result", r.getResult()));
 					}
+					r.persist(persistHandler);
 				}
 			}
 		});

feeder/src/main/java/org/entcore/feeder/csv/CsvReport.java
@@ -42,7 +42,9 @@ import static fr.wseduc.webutils.Utils.isNotEmpty;
 public class CsvReport extends Report {
 
 	private static final String MAPPINGS = "mappings";
+	private static final String CLASSES_MAPPING = "classesMapping";
 	private static final String HEADERS = "headers";
+	public static final String KEYS_CLEANED = "keysCleaned";
 	private final Vertx vertx;
 	protected final ProfileColumnsMapper columnsMapper;
 
@@ -54,6 +56,9 @@ public class CsvReport extends Report {
 			importInfos.remove("id");
 		}
 		result.mergeIn(importInfos);
+		if (result.getBoolean(KEYS_CLEANED, false)) {
+			uncleanKeys();
+		}
 		this.vertx = vertx;
 		this.columnsMapper = new ProfileColumnsMapper(getMappings());
 	}
@@ -84,6 +89,24 @@ public class CsvReport extends Report {
 		if (mappings != null && mappings.size() > 0) {
 			result.put(MAPPINGS, mappings);
 		}
+	}
+
+	public void setClassesMapping(JsonObject mapping) {
+		if (mapping != null && mapping.size() > 0) {
+			result.put(CLASSES_MAPPING, mapping);
+		}
+	}
+
+	public JsonObject getClassesMapping(String profile) {
+		final JsonObject cm = result.getJsonObject(CLASSES_MAPPING);
+		if (cm != null) {
+			return cm.getJsonObject(profile);
+		}
+		return null;
+	}
+
+	public JsonObject getClassesMappings() {
+		return result.getJsonObject(CLASSES_MAPPING);
 	}
 
 	public void exportFiles(final Handler<AsyncResult<String>> handler) {
@@ -176,6 +199,26 @@ public class CsvReport extends Report {
 	@Override
 	public String getSource() {
 		return "CSV";
+	}
+
+	public String getStructureExternalId() {
+		return result.getString("structureExternalId");
+	}
+
+	@Override
+	protected void cleanKeys() {
+		int count = 0;
+		count += cleanAttributeKeys(getClassesMappings());
+		count += cleanAttributeKeys(getMappings());
+		if (count > 0) {
+			result.put(KEYS_CLEANED, true);
+		}
+	}
+
+	protected void uncleanKeys() {
+		uncleanAttributeKeys(getClassesMappings());
+		uncleanAttributeKeys(getMappings());
+		result.remove(KEYS_CLEANED);
 	}
 
 //	protected void setStructureExternalIdIfAbsent(String structureExternalId) {

feeder/src/main/java/org/entcore/feeder/csv/CsvValidator.java
@@ -48,12 +48,13 @@ import static org.entcore.feeder.utils.CSVUtil.getCsvReader;
 
 public class CsvValidator extends CsvReport implements ImportValidator {
 
-	private enum CsvValidationProcessType { VALIDATE, COLUMN_MAPPING }
+	private enum CsvValidationProcessType { VALIDATE, COLUMN_MAPPING, CLASSES_MAPPING }
 	private final Vertx vertx;
 	private String structureId;
 	private final MappingFinder mappingFinder;
 	private boolean findUsersEnabled = true;
 	private final Map<String, String> classesNamesMapping = new HashMap<>();
+	private final Map<String, Set<String>> profilesClassesMapping = new HashMap<>();
 	public static final Map<String, Validator> profiles;
 	private final Map<String, String> studentExternalIdMapping = new HashMap<>();
 	private final long defaultStudentSeed;
@@ -78,6 +79,76 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 
 	public void columnsMapping(final String p, final Handler<JsonObject> handler) {
 		process(p, CsvValidationProcessType.COLUMN_MAPPING, handler);
+	}
+
+	public void classesMapping(final String p, final Handler<JsonObject> handler) {
+		process(p, CsvValidationProcessType.CLASSES_MAPPING, new Handler<JsonObject>() {
+			@Override
+			public void handle(JsonObject event) {
+				if (isNotEmpty(getStructureExternalId())) {
+					final String query =
+							"MATCH (s:Structure {externalId:{externalId}})<-[:BELONGS]-(c:Class) " +
+							"RETURN COLLECT(DISTINCT c.name) as classes";
+					final JsonObject params = new JsonObject().put("externalId", getStructureExternalId());
+					TransactionManager.getNeo4jHelper().execute(query, params, new Handler<Message<JsonObject>>() {
+						@Override
+						public void handle(Message<JsonObject> event) {
+							final JsonArray r = event.body().getJsonArray("result");
+							if ("ok".equals(event.body().getString("status")) && r != null && r.size() > 0 && r.getJsonObject(0) != null) {
+								final JsonArray classes = r.getJsonObject(0).getJsonArray("classes");
+								mapClasses(classes);
+							} else {
+								addError("error.database.classes");
+								log.error(event.body().getString("message"));
+							}
+							handler.handle(result);
+						}
+					});
+				} else {
+					mapClasses(null);
+					handler.handle(result);
+				}
+			}
+		});
+	}
+
+	protected void mapClasses(JsonArray classes) {
+		JsonObject classesMapping = new JsonObject();
+		Set<String> stm = profilesClassesMapping.get("Student");
+		if (stm != null && stm.size() > 0) {
+			for (String profile : profiles.keySet()) {
+				if (!"Student".equals(profile)) {
+					Set<String> m = profilesClassesMapping.get(profile);
+					if (m == null || m.size() == 0) continue;
+					JsonObject jm = new JsonObject();
+					classesMapping.put(profile, jm);
+					for (String c: m) {
+						if (stm.contains(c)) {
+							jm.put(c, c);
+						} else {
+							jm.put(c, "");
+						}
+					}
+				}
+			}
+			JsonObject jm = new JsonObject();
+			classesMapping.put("Student", jm);
+			for (String s : stm) {
+				jm.put(s, "");
+			}
+		}
+		if (classes != null && classes.size() > 0) {
+			for (String attr : classesMapping.fieldNames()) {
+				JsonObject j = classesMapping.getJsonObject(attr);
+				for (String attr2 : j.fieldNames()) {
+					if (classes.contains(attr2)) {
+						j.put(attr2, attr2);
+					}
+				}
+			}
+			classesMapping.put("dbClasses", classes);
+		}
+		setClassesMapping(classesMapping);
 	}
 
 	@Override
@@ -171,6 +242,9 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 															case COLUMN_MAPPING:
 																checkColumnsMapping(file, profile, charset, h);
 																break;
+															case CLASSES_MAPPING:
+																checkClassesMapping(file, profile, charset, h);
+																break;
 														}
 													} else {
 														addError("unknown.profile");
@@ -194,6 +268,51 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 				}
 			}
 		});
+	}
+
+	private void checkClassesMapping(String path, String profile, String charset, Handler<JsonObject> handler) {
+		try {
+			CSVReader csvParser = getCsvReader(path, charset);
+
+			String[] strings;
+			final List<String> columns = new ArrayList<>();
+			final List<Integer> classesIdx = new ArrayList<>();
+			final Set<String> mapping = new HashSet<>();
+			profilesClassesMapping.put(profile, mapping);
+			int i = 0;
+			while ((strings = csvParser.readNext()) != null) {
+				if (i == 0) {
+					JsonArray invalidColumns = columnsMapper.getColumsNames(profile, strings, columns);
+					if (invalidColumns.size() > 0 ) {
+						parseErrors("invalid.column", invalidColumns, profile, handler);
+						return;
+					} else if (!columns.contains("classes") && !columns.contains("childClasses")) {
+						handler.handle(result);
+						return;
+					} else {
+						int j = 0;
+						for (String column : columns) {
+							if ("classes".equals(column) || "childClasses".equals(column)) {
+								classesIdx.add(j);
+							}
+							j++;
+						}
+					}
+				} else {
+					for (Integer idx : classesIdx) {
+						if (isNotEmpty(strings[idx].trim())) {
+							mapping.add(strings[idx].trim());
+						}
+					}
+				}
+				i++;
+			}
+		} catch (Exception e) {
+			addError(profile, "csv.exception");
+			log.error("csv.exception", e);
+		} finally {
+			handler.handle(result);
+		}
 	}
 
 	private void checkColumnsMapping(String path, String profile, String charset, Handler<JsonObject> handler) {
@@ -342,6 +461,7 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 				final JsonObject checkChildExists = new JsonObject();
 //				setStructureExternalIdIfAbsent(structure.getExternalId());
 				try {
+					final JsonObject classMapping = getClassesMapping(profile);
 					CSVReader csvParser = getCsvReader(path, charset, 1);
 					final int nbColumns = columns.size();
 					String[] strings;
@@ -370,8 +490,16 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 								return;
 							}
 							final String c = columns.get(j);
-							final String v = strings[j].trim();
+							final String v;
+							if ("classes".equals(c) && classMapping != null) {
+								v = getOrElse(classMapping.getString(strings[j].trim()), strings[j].trim(), false);
+							} else {
+								v = strings[j].trim();
+							}
+							//if ((v.isEmpty() && !"childUsername".equals(c)) ||
+							//		(v.isEmpty() && "childUsername".equals(c) && strings[j+1].trim().isEmpty())) continue;
 							if (v.isEmpty() && !c.startsWith("child")) continue;
+
 							switch (validator.getType(c)) {
 								case "string":
 									if ("birthDate".equals(c)) {

feeder/src/main/java/org/entcore/feeder/utils/Report.java
@@ -162,7 +162,7 @@ public class Report {
 	}
 
 	public JsonObject getResult() {
-		return result;
+		return result.copy();
 	}
 
 	public void setUsersExternalId(JsonArray usersExternalIds) {
@@ -386,7 +386,39 @@ public class Report {
 	public void setMappings(JsonObject mappings) {
 		if (mappings != null && mappings.size() > 0) {
 			result.put(MAPPINGS, mappings);
+		}
+	}
 
+	protected int cleanAttributeKeys(JsonObject attribute) {
+		int count = 0;
+		if (attribute != null) {
+			for (String attr : attribute.fieldNames()) {
+				JsonObject j = attribute.getJsonObject(attr);
+				if (j != null) {
+					for (String attr2 : j.copy().fieldNames()) {
+						if (attr2.contains(".")) {
+							count++;
+							j.put(attr2.replaceAll("\\.", "_|_"), (String) j.remove(attr2));
+						}
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	protected void uncleanAttributeKeys(JsonObject attribute) {
+		if (attribute != null) {
+			for (String attr : attribute.fieldNames()) {
+				JsonObject j = attribute.getJsonObject(attr);
+				if (j != null) {
+					for (String attr2 : j.copy().fieldNames()) {
+						if (attr2.contains("_|_")) {
+							j.put(attr2.replaceAll("_\\|_", "."), (String) j.remove(attr2));
+						}
+					}
+				}
+			}
 		}
 	}
 